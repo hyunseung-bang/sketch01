dae0388c02adb0d15dfb938864b1b9a6418b1ee6
hyunseung-bang <92072533+hyunseung-bang@users.noreply.github.com>
2022-06-20 13:10:07 +0900

    Create com12.js

---
+++ com12.js
@@ -0,0 +1,31 @@
+
+
+$(function () {
+    try {
+      alert("git-10");
+      initLanguage();
+      
+     
+    } catch(e) {
+        alert(e);
+    }
+});
+
+<!-- 언어변경 -->
+function initLanguage() {
+alert("initLanguage");
+alert("g_lang = " + g_lang);
+    if( !g_lang || g_lang === "ko") {
+        $("#language-ko").hide();
+        $("#language-en").show();
+    } else {
+        $("#language-ko").show();
+        $("#language-en").hide();
+    }
+    $("#language-ko").click(function() {
+        $.get('/language-change/ko').done(function(result) { location.reload(); });
+    });
+    $('#language-en').click(function() {
+        $.get('/language-change/en').done(function(result) { location.reload(); });
+    });
+}
