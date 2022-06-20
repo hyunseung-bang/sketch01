71db063ed7ad3807ac1f02585ef8c2d0cb8a14a5
hyunseung-bang <92072533+hyunseung-bang@users.noreply.github.com>
2022-06-20 13:04:35 +0900

    Create com11.js

---
+++ com11.js
@@ -0,0 +1,38 @@
+alert("git-10");
+
+$(function () {
+    try {
+      initLanguage();
+      loadNotice();
+      
+    } catch(e) {
+        alert(e);
+    }
+});
+
+<!-- 언어변경 -->
+function initLanguage() {
+    if( !g_lang || g_lang === "ko") {
+        $("#language-ko").hide();
+        $("#language-en").show();
+    } else {
+        $("#language-ko").show();
+        $("#language-en").hide();
+    }
+    $("#language-ko").click(function() {
+        $.get('/language-change/ko').done(function(result) { location.reload(); });
+    });
+    $('#language-en').click(function() {
+        $.get('/language-change/en').done(function(result) { location.reload(); });
+    });
+}
+<!-- 회원가입유의사항 -->
+function loadNotice() {
+    alert("loadNotice");
+    $.get("/member-join-notice").done((response) => {
+        alert(22);
+        $(".mo-note").html('<img src="' + response.data.appImgPath + '"/>');
+        alert(33);
+        $(".pc-note").html('<img src="' + response.data.webImgPath + '"/>');
+    });
+}
