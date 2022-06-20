d085b4090b9d955c1f80b9ee6a40ab97ebf7e9bb
hyunseung-bang <92072533+hyunseung-bang@users.noreply.github.com>
2022-06-20 12:06:33 +0900

    Update com.js

--- com.js
+++ com.js
@@ -1,1 +1,26 @@
-alert(1111);
+alert("git-1");
+
+$(function () {
+    try {
+      initLanguage();
+
+    } catch(e) {
+        alert(e);
+    }
+});
+
+function initLanguage() {
+    if( !g_lang || g_lang === "ko") {
+        $("#language-ko").hide();
+        $("#language-en").show();
+    } else {
+        $("#language-ko").show();
+        $("#language-en").hide();
+    }
+    $("#language-ko").click(function() {
+        $.get('/language-change/ko').done(function(result) { location.reload(); });
+    });
+    $('#language-en').click(function() {
+        $.get('/language-change/en').done(function(result) { location.reload(); });
+    });
+}
